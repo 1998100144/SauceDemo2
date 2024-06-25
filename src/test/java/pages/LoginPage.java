892cecece06d282eb360f489c77933f6556499b3
Kasimova Zarina <zosyas14@gmail.com>
2024-06-25 22:59:28 +0400

    Add page

--- src/test/java/pages/LoginPage.java
+++ src/test/java/pages/LoginPage.java
@@ -9,6 +9,10 @@ public class LoginPage {
     private final By USERNAME_INPUT = By.id("user-name");
     private final By PASSWORD_INPUT = By.id("password");
     private final By LOGIN_BUTTON = By.id("login-button");
+    private final By EMPTY_USER_INPUT_LOGIN = By.cssSelector("[class=product_label]");
+    private final By ERROR_NO_PASSWORD = By.xpath("//h3[contains(text(), 'Password is required')]");
+    private final By ERROR_INCORRECT_PASSWORD = By.xpath("//h3[contains(text(), 'password do not match any user in this service')]");
+    private final By ERROR_LOCKED_USER = By.xpath("//h3[contains(text(), 'user has been locked out')]");
 
     public LoginPage(WebDriver driver) {
         this.driver = driver;
@@ -23,4 +27,17 @@ public class LoginPage {
         driver.findElement(PASSWORD_INPUT).sendKeys(password);
         driver.findElement(LOGIN_BUTTON).submit();
     }
+    public String getEmptyUserInputLogin() {
+        return driver.findElement(EMPTY_USER_INPUT_LOGIN).getText();
+    }
+    public String getErrorNoPassword() {
+        return driver.findElement(ERROR_NO_PASSWORD).getText();
+    }
+    public String getIncorrectPassword() {
+        return driver.findElement(ERROR_INCORRECT_PASSWORD).getText();
+    }
+    public String getLockedUser() {
+        return driver.findElement(ERROR_LOCKED_USER).getText();
+    }
+
 }
--- src/test/java/tests/LoginTest2.java
+++ src/test/java/tests/LoginTest2.java
@@ -9,34 +9,34 @@ public class LoginTest2 extends BaseTest {
     public void correctLogin() {
         loginPage.open();
         loginPage.login("standard_user", "secret_sauce");
-        assertEquals(driver.findElement(By.cssSelector("[class=product_label]")).getText(), "Products");
+        assertEquals(loginPage.getEmptyUserInputLogin(), "Products");
     }
 
     @Test
     public void emptyUserInputLogin() {
         loginPage.open();
         loginPage.login("", "secret_sauce");
-        assertEquals(driver.findElement(By.cssSelector("[class=product_label]")).getText(), "Products");
+        assertEquals(loginPage.getEmptyUserInputLogin(), "Products");
     }
 
     @Test
     public void noPassword() {
         loginPage.open();
         loginPage.login("standard_user", "");
-        assertEquals(driver.findElement(By.xpath("//h3[contains(text(), 'Password is required')]")).getText(), "Epic sadface: Password is required");
+        assertEquals(loginPage.getErrorNoPassword(), "Epic sadface: Password is required");
     }
 
     @Test
-    public void lockedUse() {
+    public void lockedUser() {
         loginPage.open();
         loginPage.login("locked_out_user", "secret_sauce");
-        assertEquals(driver.findElement(By.xpath("//h3[contains(text(), 'user has been locked out')]")).getText(), "Epic sadface: Sorry, this user has been locked out.");
+        assertEquals(loginPage.getLockedUser(), "Epic sadface: Sorry, this user has been locked out.");
     }
 
     @Test
     public void incorrectPassword() {
         loginPage.open();
         loginPage.login("standard_user", "123");
-        assertEquals(driver.findElement(By.xpath("//h3[contains(text(), 'password do not match any user in this service')]")).getText(), "Epic sadface: Username and password do not match any user in this service");
+        assertEquals(loginPage.getIncorrectPassword(), "Epic sadface: Username and password do not match any user in this service");
     }
 }

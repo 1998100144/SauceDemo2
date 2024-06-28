bb569b9432ee12440c394450a6f319426af982cd
Kasimova Zarina <zosyas14@gmail.com>
2024-06-28 19:12:54 +0400

    Delete Webelement and add locator ERROR_MESSAGE

--- src/test/java/pages/LoginPage.java
+++ src/test/java/pages/LoginPage.java
@@ -2,7 +2,6 @@ package pages;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 
 public class LoginPage {
 
@@ -11,6 +10,7 @@ public class LoginPage {
     private final By PASSWORD_INPUT = By.id("password");
     private final By LOGIN_BUTTON = By.id("login-button");
     private final By EMPTY_USER_INPUT_LOGIN = By.cssSelector("[class=product_label]");
+    private final By ERROR_MESSAGE = By.xpath("//h3[@data-test='error']");
 
     public LoginPage(WebDriver driver) {
         this.driver = driver;
@@ -29,7 +29,6 @@ public class LoginPage {
         return driver.findElement(EMPTY_USER_INPUT_LOGIN).getText();
     }
     public String getErrorMessage() {
-        WebElement errorMessage = driver.findElement(By.xpath("//h3[@data-test='error']"));
-        return errorMessage.getText();
+        return driver.findElement(ERROR_MESSAGE).getText();
     }
 }
